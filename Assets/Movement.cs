85fe5b667d31f84f7aa28b32091eca3da7de0ffc
Daniel Hampikian <danielhampikian@gmail.com>
2018-07-27 09:01:03 -0600

    Revert "initial push"
    This reverts commit 7f90e5685afd2cac942adc7e1e09c1747c77156f.

---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 100f;
+    public float turnSpeed = 5f;
+	void Start () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * turnSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
